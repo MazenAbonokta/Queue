28a4955c31f00cd2eadaf40d60a228eb6f22cc57
Mazen abonoktah <mazen.abono7tah@gmail.com>
2024-10-08 00:46:52 +0400

    init

---
+++ src/test/java/com/dlj4/tech/queue/QueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlj4.tech.queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
